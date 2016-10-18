43e035d704186ddff5f40705f60e14bcdda08c00
Andy Joiner <andy.joiner@viewpoint.com>
2016-10-18 13:52:20 +0100

    The Newspaper metaphor for laying our code.

---
+++ CleanCode/NewspaperCodeLayout/Pizza.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza.CleanCode.NewspaperCodeLayout
+{
+    class Pizza
+    {
+        /// <summary>
+        /// Problem: Taking the newspaper layout literally, AddCheese() would come after MakeMargaritaPizza()
+        /// but that would break the rule of not mixing levels of abstraction.
+        /// </summary>
+
+        //Pizza level of abstraction
+        void MakeMargaritaPizza()
+        {
+            AddCheese();
+            AddTomatoPaste();
+        }
+
+        void MakePepperoniPizza()
+        {
+            AddCheese();
+            AddTomatoPaste();
+            AddPepperoni();
+        }
+
+        //Ingredient level of abstraction, in caller order, if possible.
+        private void AddCheese()
+        {
+        }
+
+        private void AddTomatoPaste()
+        {
+        }
+
+        private void AddPepperoni()
+        {
+        }
+    }
+}
