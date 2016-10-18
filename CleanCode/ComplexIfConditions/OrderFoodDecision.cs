bd185e25849f552891d6f263d05c38122fa208ee
Andy Joiner <andy.joiner@viewpoint.com>
2016-10-18 14:15:36 +0100

    Ensapsulating if statement conditions

---
+++ CleanCode/ComplexIfConditions/OrderFoodDecision.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza.CleanCode.ComplexIfConditions
+{
+    class OrderFoodDecision
+    {
+        void DecideWithInlineLogic()
+        {
+            if (IsRaining() || Meetings.Count() > 5)
+            {
+                ChooseFullLoadedPizza();
+            }
+            else
+            {
+                ChooseSalad();
+            }
+        }
+
+        void DecideWithEncapsulatedLogic()
+        {
+            if (HadABadDay())
+            {
+                ChooseFullLoadedPizza();
+            }
+            else
+            {
+                ChooseSalad();
+            }
+        }
+
+        public bool HadABadDay()
+        {
+            return IsRaining()
+                   || (Meetings.Count() > 5);
+
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        public bool IsRaining()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Meetings => new string[] { "Disciplinary", "Paul", "Mary" };
+
+        private void ChooseFullLoadedPizza()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ChooseSalad()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
